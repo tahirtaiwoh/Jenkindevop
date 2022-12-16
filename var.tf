6f68f50e4b4b98c89d91d8b5765a6715b9bc41a9
taiwohassant <106849223+taiwohassant@users.noreply.github.com>
2022-12-16 12:09:08 -0500

    keypair update

--- var.tf
+++ var.tf
@@ -1,7 +1,7 @@
 variable "key_pair_name" {
     type = string
     description = "keypair to utilize"
-    default = "vamsi-chunduru"
+    default = "Auguest-10-keypair.pem"
 }
 
 # variable "vpc_id" {
